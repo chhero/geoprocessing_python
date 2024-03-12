9a3bf49f30e20da2dc952dabcf01f3a1eb62719c
chhero <131651296+chhero@users.noreply.github.com>
2024-03-13 01:59:23 +0800

    Update chapter_3.py

--- chapter_3.py
+++ chapter_3.py
@@ -1,1 +1,18 @@
 
+######## Git Guide Clemente - Geomatics #########
+
+#Requirements 
+#Python Environment - gdal - osgeo - ospybook - data-source (anaconda / jupyter - optional)
+
+#### Links #####
+
+#Installing Python and Environment
+# 1. https://phoenixnap.com/kb/how-to-install-python-3-windows\
+# Follow all the instructions
+# 2. Install Libraries
+
+pip install gdal
+pip install osgeo
+pip install numpy
+pip install matplotlib
+
